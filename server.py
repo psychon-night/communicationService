daabd83760fdbc4e83c711c7e939a2c074c455c6
EmmyKit <49412250+247086@users.noreply.github.com>
2021-11-30 11:05:04 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,23 +1,38 @@
-# Server version 1.0.3
+# Server version 1.0.4
 # Imports
 import socketserver, socket, time, winsound, os, locale
 from threading import Thread as td
 
 # Constants
+os.system('cls'); print("Loading...\n[###                           ]")
 DRIVELETTER = str(os.environ['WINDIR'].split(":\\")[0])
+time.sleep(0.25)
+os.system('cls'); print("Loading...\n[###############               ]")
 DATAPATH = DRIVELETTER + ":/ProgramData/PDS.comSoft"
+time.sleep(0.25)
+os.system('cls'); print("Loading...\n[#######################       ]")
 try:
     VERSION = open(DATAPATH + "/softwareVersion", "r").read()
+    
+    time.sleep(0.25)
+    os.system('cls'); print("Loading...\n[##############################]")
+    time.sleep(1)
 except:
     os.system('cls'); print("[STARTUP FAILED: CRITICAL FILES MISSING]")
     time.sleep(999)
 
 # Print the version info
+os.system('cls')
+time.sleep(0.75)
 print("ComSoft Server " + VERSION + "\n")
 
 
 # Let the user know that the server is being started
-print("Starting server (" + str(locale.getlocale()) + ")")
+if (os.path.isfile(DATAPATH + "/mute") == False):
+    print("Starting server (" + str(locale.getlocale()) + ")")
+
+else:
+    print("Starting server (" + str(locale.getlocale()) + ") in silent mode - use 'mute' to enable sound")
 time.sleep(2.5) # Gives the main thread enough time to initialize
 
 
@@ -43,13 +58,13 @@ class Handler(socketserver.BaseRequestHandler):
 
             forward(self.data) # Forward it to other users - this might take a moment if there are a lot to cycle through (especially on slow network conditions)
             print(self.client_address[0] + " connected") # Print locally
-            winsound.MessageBeep(winsound.MB_ICONEXCLAMATION) # Play notification sound
+            playsound(winsound.MB_ICONASTERISK) # Play notification sound
 
 
         else: # The user is not new
             if not ("has disconnected" in self.data.decode() or "kicked for spam" in self.data.decode()): # The user isn't trying to disconnect
                 print("client@" + self.client_address[0] + "~ " + self.data.decode()) # Print on the local display
-                winsound.MessageBeep(winsound.MB_ICONEXCLAMATION) # Notification sound
+                playsound(winsound.MB_ICONEXCLAMATION) # Notification sound
                 forward(self.data) # Forward it to other users
 
 
@@ -57,7 +72,7 @@ class Handler(socketserver.BaseRequestHandler):
                 user = self.client_address[0] # The user who sent the message
                 forward(self.data) # Tell other users that someone has left
                 print("client@" + user + " disconnected") # Print on the local display
-                winsound.MessageBeep(winsound.MB_ICONASTERISK) # Notification sound
+                playsound(winsound.MB_ICONEXCLAMATION) # Notification sound
 
                 client_list.remove(self.client_address[0]) # Remove the person who wants to be removed 
             
@@ -65,7 +80,7 @@ class Handler(socketserver.BaseRequestHandler):
                 user = self.client_address[0] # The user who sent the message
                 forward(self.data) # Tell other users that someone has been kicked
                 print("client@" + user + " kicked for spam") # Print on the local display
-                winsound.MessageBeep(winsound.MB_ICONASTERISK) # Notification sound
+                playsound(winsound.MB_ICONASTERISK) # Play the notificatin sound
 
                 client_list.remove(self.client_address[0]) # Remove the person who got
             
@@ -80,35 +95,56 @@ def forward(data): # Forward incoming messages to other users
     index = 0
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        while (forwarded != (len(client_list) - 1)):
-            if (client_list[index] != socket.gethostname()):
-                sock.connect((client_list[index], 1500)) # Connect to the desired endpoint
+        while (forwarded <= (len(client_list) - 1)):
+            if (client_list[index] != serverIP):
+                dest = client_list.pop(index)
+                sock.connect((str(dest), 1500)) # Connect to the desired endpoint
                 sock.sendall(data) # Send the message
                 sock.close() # Close the connection
+                client_list.append(dest)
+                time.sleep(0.025)
 
             # Update the loop variables
             forwarded += 1
             index += 1
+
+def playsound(type):
+    if (os.path.isfile(DATAPATH + "/mute") == False):
+        winsound.MessageBeep(type)
 
 
 def serverCommandProcessor():
     time.sleep(0.75)
     command = input("")
     if (command == "help"):
-        print("Supported commands: 'help', 'shutdown', 'disableForwarding'")
+        print("Supported commands: 'help', 'shutdown', 'listClients', 'mute'") # 'disableForwarding' for future
+        print(" > 'help': get this message\n > 'shutdown': shut down the server - doesn't warn clients!\n > 'listClients': list the connected clients\n > 'mute': mute notification sounds - persists across restart. Use again to unmute")
     
     elif (command == "shutdown"): # This command shuts down the server
         print("Shutting down server...")
         time.sleep(0.75)
 
-        forward("[Server] Shutdown in progress") # Tell the other users that the server is in shutdown
-        time.sleep(0.75)
+        os.abort() # Forcefully close out
+    
+    elif (command == "listClients"):
+        print(client_list)
 
-        with socketserver.TCPServer((serverIP, 1500), Handler) as server: # Connect to itself
-            server.shutdown() # Shut down the server softly
+    elif (command == "mute"):
+        if (os.path.isfile(DATAPATH + "/mute") == False):
+            open(DATAPATH + "/mute", "x").close() # Adds the file that mutes the notifications
+            print("\x10 Notifications disabled")
+        else:
+            os.remove(DATAPATH + "/mute") # Removes the file that mutes the notifications
+            print("\x10 Notifications enabled")
 
-        time.sleep(0.75)
-        exit()
+    elif (command == "forwardRAW"): # DANGEROUS COMMAND. Not listed by using help
+        print("Type text to send")
+        text = input("")
+
+        forward("[Server \x10] -- " + text)
+    
+    else:
+        print("Command not found. Did you type it right? [Use 'help' to show safe commands]")
     serverCommandProcessor()
 
 
